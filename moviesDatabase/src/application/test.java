6af153cab34a4415ef565342721768a9ac3d7727
Sofia Khan <40192949+sofiakhn@users.noreply.github.com>
2020-07-20 22:26:37 -0700

    Add files via upload

---
+++ moviesDatabase/src/application/test.java
@@ -0,0 +1,41 @@
+package application;
+import javafx.application.Application;  
+import javafx.event.ActionEvent;  
+import javafx.event.EventHandler;  
+import javafx.scene.Scene;  
+import javafx.scene.control.Button;  
+import javafx.stage.Stage;  
+import javafx.scene.layout.StackPane; 
+
+// Run this class to see a simple Hello World button JAVAFx. 
+// Just for demonstration purposes. 
+
+public class test extends Application{
+	  
+	    @Override  
+	    public void start(Stage primaryStage) throws Exception {  
+	        // TODO Auto-generated method stub  
+	        Button btn1=new Button("Say, Hello World");  
+	        btn1.setOnAction(new EventHandler<ActionEvent>() {  
+	              
+	            @Override  
+	            public void handle(ActionEvent arg0) {  
+	                // TODO Auto-generated method stub  
+	                System.out.println("hello world");  
+	            }  
+	        });  
+	        StackPane root=new StackPane();  
+	        root.getChildren().add(btn1);  
+	        Scene scene=new Scene(root,600,400);      
+	        primaryStage.setTitle("First JavaFX Application");  
+	        primaryStage.setScene(scene);  
+	        primaryStage.show();  
+	    }  
+	    
+	    public static void main (String[] args)  
+	    {  
+	        launch(args);  
+	    }  
+	  
+}  
+
