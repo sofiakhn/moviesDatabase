9fb3fe44f5218e4b6a49825e968245256ac3e72d
Manya Waldia <waldia@wisc.edu>
2020-07-22 22:33:02 -0500

    hi

--- moviesDatabase/src/application/home.java
+++ moviesDatabase/src/application/home.java
@@ -1,25 +1,63 @@
 package application;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+public class home extends Application {
 
-public class home extends Application{
+    Button button = new Button();
+    @FXML
+    private AnchorPane ap;
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
+    // one method which checks the id of all buttons
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("home.fxml"));
-        VBox vbox = loader.<VBox>load();
-
-        Scene scene = new Scene(vbox);
+    public void start(Stage primaryStage) throws IOException {
+        
+        Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
+        Scene scene = new Scene(root);
+        // button.setOnAction(event -> checkID(event, button));
+        primaryStage.setTitle("Movies Database");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        Button button = new Button();
+        button.setId(value);
+        
+        if (button.getId().equals("movieadd")) {
+            button.setOnAction(e -> {
+                try {
+                    // root = FXMLLoader.load(getClass().getResource("addMovie.fxml"));
+                    Scene search =FXMLLoader.load(getClass().getResource("addMovie.fxml"));
+                    primaryStage.setScene(search);
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+
+            });
+        }
+        else{
+
+        }
+
     }
+    
+    
+	public static void main(String[] args) {
+		launch(args);
+	}
+
 }
